2fdf99ede29e0c08e6877b8127e08ee0ac6ad5d0
Yuriy2056757 <2056757@talnet.nl>
2019-03-19 21:19:52 +0100

    Day 5

--- core/src/com/kilobolt/screens/GameScreen.java
+++ core/src/com/kilobolt/screens/GameScreen.java
@@ -2,55 +2,62 @@ package com.kilobolt.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.GL20;
 import com.kilobolt.gameworld.GameRenderer;
 import com.kilobolt.gameworld.GameWorld;
 
 public class GameScreen implements Screen {
 
-    private GameWorld world;
-    private GameRenderer renderer;
+  private GameWorld world;
+  private GameRenderer renderer;
 
-    public GameScreen() {
-        Gdx.app.log("GameScreen", "Attached");
-        world = new GameWorld();
-        renderer = new GameRenderer(world);
-    }
+  public GameScreen() {
+    float screenWidth = Gdx.graphics.getWidth();
+    float screenHeight = Gdx.graphics.getHeight();
+    float gameWidth = 136;
+    float gameHeight = screenHeight / (screenWidth / gameWidth);
 
-    @Override
-    public void render(float delta) {
-        world.update(delta);
-        renderer.render();
-    }
+    int midPointY = (int) (gameHeight / 2);
 
-    @Override
-    public void resize(int width, int height) {
+    world = new GameWorld(midPointY);
+    renderer = new GameRenderer(world);
 
-    }
+    Gdx.input.setInputProcessor(new InputHandler(world.getBird()));
+  }
 
-    @Override
-    public void show() {
-        Gdx.app.log("GameScreen", "show called");
-    }
+  @Override
+  public void render(float delta) {
+    world.update(delta);
+    renderer.render();
+  }
 
-    @Override
-    public void hide() {
-        Gdx.app.log("GameScreen", "hide called");
-    }
+  @Override
+  public void resize(int width, int height) {
 
-    @Override
-    public void pause() {
-        Gdx.app.log("GameScreen", "pause called");
-    }
+  }
 
-    @Override
-    public void resume() {
-        Gdx.app.log("GameScreen", "resume called");
-    }
+  @Override
+  public void show() {
+    Gdx.app.log("GameScreen", "show called");
+  }
 
-    @Override
-    public void dispose() {
-        // Leave blank
-    }
+  @Override
+  public void hide() {
+    Gdx.app.log("GameScreen", "hide called");
+  }
+
+  @Override
+  public void pause() {
+    Gdx.app.log("GameScreen", "pause called");
+  }
+
+  @Override
+  public void resume() {
+    Gdx.app.log("GameScreen", "resume called");
+  }
+
+  @Override
+  public void dispose() {
+    // Leave blank
+  }
 
 }
