b5f95aa989fc5e5014918d02b19323f1e2b370d9
Yuriy2056757 <2056757@talnet.nl>
2019-04-03 20:57:28 +0200

    Day 8 - Checking for Collision

--- core/src/com/kilobolt/gameobjects/ScrollHandler.java
+++ core/src/com/kilobolt/gameobjects/ScrollHandler.java
@@ -65,4 +65,17 @@ public class ScrollHandler {
   public Pipe getPipe3() {
     return pipe3;
   }
+
+  public void stop() {
+    frontGrass.stop();
+    backGrass.stop();
+    pipe1.stop();
+    pipe2.stop();
+    pipe3.stop();
+  }
+
+  // Return true if ANY pipe hits the bird.
+  public boolean collides(Bird bird) {
+    return (pipe1.collides(bird) || pipe2.collides(bird) || pipe3.collides(bird));
+  }
 }
--- core/src/com/kilobolt/gameobjects/Scrollable.java
+++ core/src/com/kilobolt/gameobjects/Scrollable.java
@@ -58,4 +58,8 @@ public class Scrollable {
     public int getHeight() {
         return height;
     }
+
+    public void stop() {
+        velocity.x = 0;
+    }
 }
